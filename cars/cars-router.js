a495faef2ed3394abdbbdbf667873bd8a672a337
Alec Dye <alecdye@gmail.com>
2020-04-14 22:40:43 -0700

    Building GET and POST endpoints

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -8,13 +8,49 @@ const db = knex(knewfile.development);
 const router = express.Router();
 
 // API Endpoints
+// GET ---
+router.get("/", (req, res) => {
+  // ("cars") = the name of the table inside the database "car-dealer"
+  db("cars")
+    .then((cars) => {
+      res.status(201).json(cars);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed to retrieve cars", error });
+    });
+});
 
-// GET
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db("cars")
+    .where({ id })
+    .first()
+    .then((car) => {
+      res.status(200).json(car);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Failed to retrieve car", error });
+    });
+});
 
-// POST
+// POST ---
+router.post("/", (req, res) => {
+  const carData = req.body;
+  db("cars")
+    .insert(carData)
+    .then((ids) => {
+      db("cars")
+        .where({ id: ids[0] })
+        .then((newCar) => {
+          res.status(201).json(newCar);
+        });
+    })
+    .catch((error) =>
+      res.status(500).json({ message: "Failed to store data", error })
+    );
+});
+// PUT ---
 
-// PUT
-
-// DELETE
+// DELETE ---
 
 module.exports = router;

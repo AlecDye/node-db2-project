737a3d2665ff7116a5eab0ca252c86e62a9b7a99
Alec Dye <alecdye@gmail.com>
2020-04-15 14:40:06 -0700

    Debugging secondary migration

--- data/migrations/20200415124721_add_secondary_columns.js
+++ data/migrations/20200415124721_add_secondary_columns.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.table("cars", (tbl) => {
-    tbl.increments("id");
+    // tbl.increments("id");
     tbl.string("Transmission");
     tbl.string("TitleStatus");
   });

8d43d91d3f95edd08f2dac6845a927c56fc21dd2
Alec Dye <alecdye@gmail.com>
2020-04-15 12:50:05 -0700

    Building secondary columns

---
+++ data/migrations/20200415124721_add_secondary_columns.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.table("cars", (tbl) => {
+    tbl.increments("id");
+    tbl.string("Transmission");
+    tbl.string("TitleStatus");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.table("cars", (tbl) => {
+    tbl.dropColumn("TitleStatus");
+    tbl.dropColumn("Transmission");
+  });
+};
